429c1845db660827cc97349a1d5cf3b48cf381e9
Ruslan Ishchenko <ichenko909@gmail.com>
2024-06-06 16:29:25 +0200

    Lecture 6: пример лабы

---
+++ lecture6/src/test/java/org/example/impl/StringCharactersSetTest.java
@@ -0,0 +1,20 @@
+package org.example.impl;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class CharactersSetImplTest {
+@Test
+    public void intersectCharactersSet(){
+    String []arr1 = {"a","b","c","d","e"};
+    StringCharactersSet set1 = StringCharactersSet.fromArray("a","b","c","d","e");
+//    StringCharactersSet set1 = new StringCharactersSet("a","b","c","d","e");not possible as constructor is private
+    StringCharactersSet set2 = StringCharactersSet.fromSet(Set.of("k","l","a","b","c"));
+
+    assertThat(set1.intersect(set2).getCharacters()).isEqualTo(Set.of("a","b","c"));
+    assertThat(set2.intersect(set1).getCharacters()).isEqualTo(Set.of("a","b","c"));
+}
+}
